30c540ca2db4cb83138e569781fdfe3248086222
S. M. Abrar Jahin <abrarjahin@live.com>
2015-10-05 06:22:43 +0600

    All Done with AJAX Dynamic responsive chart

--- ajax.php
+++ ajax.php
@@ -1,41 +1,44 @@
 <?php
-      $JSON = array(
-					array(
-								'time_year'		=> 2005,
-								'time_month'	=> 10,
-								'time_day'		=> 12,
-								'time_hour'		=> 2,
-								'time_min'		=> 30,
-								'time_sec'		=> 12,
-								'total_user_no'	=> 100 
-							),
-					array(
-								'time_year'		=> 2005,
-								'time_month'	=> 11,
-								'time_day'		=> 12,
-								'time_hour'		=> 2,
-								'time_min'		=> 30,
-								'time_sec'		=> 12,
-								'total_user_no'	=> 190 
-							),
-					array(
-								'time_year'		=> 2005,
-								'time_month'	=> 12,
-								'time_day'		=> 12,
-								'time_hour'		=> 2,
-								'time_min'		=> 30,
-								'time_sec'		=> 12,
-								'total_user_no'	=> 90 
-							),
-					array(
-								'time_year'		=> 2006,
-								'time_month'	=> 1,
-								'time_day'		=> 12,
-								'time_hour'		=> 2,
-								'time_min'		=> 30,
-								'time_sec'		=> 12,
-								'total_user_no'	=> 180 
-							)
-                    );
-      echo json_encode($JSON);
+
+	function random_one_entry($time_day)
+	{
+	    return array(
+						'time_year'		=> 2015,
+						'time_month'	=> 10,
+						'time_day'		=> $time_day,
+						'time_hour'		=> rand(0,24),
+						'time_min'		=> rand(0,60),
+						'time_sec'		=> rand(0,60),
+						'value'			=> rand()
+					);
+	}
+
+	function random_single_data_generate($max_no_of_dataset_per_data)
+	{
+		$data_to_return=[];
+		if($max_no_of_dataset_per_data<2)$max_no_of_dataset_per_data=2;
+		$total_no_of_data = rand(2,$max_no_of_dataset_per_data);
+		for ($x = 1; $x <= $total_no_of_data; $x++)
+		{
+		    $data_to_return[] = random_one_entry($x);
+		}
+		return $data_to_return;
+	}
+
+	function random_data_generate($max_no_of_lines_to_draw,$max_no_of_dataset_per_data)
+	{
+		$data_to_return=[];
+		if($max_no_of_lines_to_draw<1)$max_no_of_lines_to_draw=1;
+		$total_no_of_data = rand(1,$max_no_of_lines_to_draw);			//Total no of lines
+		for ($x = 1; $x <= $total_no_of_data; $x++)
+		{
+		    $data_to_return[] = array(
+		    							'name' => 'Line No '.$x,
+    									'data' => random_single_data_generate($max_no_of_dataset_per_data)
+		    						);
+		}
+		return $data_to_return;
+	}
+
+    echo json_encode(random_data_generate(10,30));			//2 = no of line (max), 3=no of data per line (max)
 ?>
--- js/chart.js
+++ js/chart.js
@@ -1,119 +1,154 @@
-google.load('visualization', '1.1', {packages: ['line', 'corechart']});
-
-
-function get_ajax_data_for_total_user()
+$(function ()                                               //Document Ready
 {
-    var data = new google.visualization.DataTable();
-    data.addColumn('date', 'Time');
-    data.addColumn('number', "Total Users");
-
-    var jsonData = $.ajax({
-                              url: "ajax.php",
-                              dataType: "json",
-                              async: false
-                            }).responseText;
-    var temp_AJAX_data=[];
-    $.each($.parseJSON(jsonData), function(index, element)
+    if($("#container").length != 0)     //if the div exists
     {
-        var time_year=0,time_month=0,time_day=0,time_hour=0,time_min=0,time_sec=0,total_user_no=0;
-        //console.log(index+" Start");
-        $.each(element,function(key,value)
+        drawLineChart('container','ajax.php');
+    }
+});
+
+setInterval(function()                      //Set Auto Refresh in every 5s
+{
+    if($("#container").length != 0)     //if the div exists
+    {
+        drawLineChart('container','ajax.php');
+    }
+}, 5000);
+
+function drawLineChart(div_id,ajax_source)
+{
+    $('#'+div_id).highcharts(
+    {
+        credits:
         {
-            if(key.localeCompare('time_year')==0)
-            {
-                time_year=value;
-            }
-            else if(key.localeCompare('time_month')==0)
-            {
-                time_month=value;
-            }
-            else if(key.localeCompare('time_day')==0)
-            {
-                time_day=value;
-            }
-            else if(key.localeCompare('time_hour')==0)
-            {
-                time_hour=value;
-            }
-            else if(key.localeCompare('time_min')==0)
-            {
-                time_min=value;
-            }
-            else if(key.localeCompare('time_sec')==0)
-            {
-                time_sec=value;
-            }
-            else if(key.localeCompare('total_user_no')==0)
-            {
-                total_user_no=value;
-            }
-        });
-        temp_AJAX_data.push([new Date(time_year, time_month,time_day,time_hour,time_min,time_sec),  total_user_no]);
-    });
-
-    //console.log(temp_AJAX_data);
-    data.addRows(temp_AJAX_data);
-
-    return data;
-}
-
-function drawChart_totalUser(div_id)
-{
-    var chartDiv = document.getElementById(div_id);
-
-    var Options =
-    {
+          enabled: false
+        },
         chart:
         {
-            title: 'No of Users in Different Time'
+            type: 'spline'
         },
-        width: '100%',
-        height: '100%',
-        series:
+        plotOptions:
         {
-            // Gives each series an axis name that matches the Y-axis below.
-            0: {axis: 'Temps'},
-            1: {axis: 'Daylight'}
-        },
-        axes:
-        {
-            // Adds labels to each axis; they don't have to match the axis names.
-            y:
+            /*series:                 //Stopping the animation
             {
-                Temps: {label: 'Total Users'}
+                animation: false
+            },*/
+            spline:
+            {
+                marker:
+                {
+                    enabled: true
+                }
             }
-        }/*,
-        animation:
+        },
+        title:
         {
-            duration: 1000,
-            easing: 'out',
-        }*/
-    };
-
-    var materialChart = new google.charts.Line(chartDiv);
-    materialChart.draw(get_ajax_data_for_total_user(), Options);
+            text: 'No of Users vs Time'
+        },
+        subtitle:
+        {
+            text: 'Toatl no of attenders in this webinar'
+        },
+        xAxis:
+        {
+            type: 'datetime',
+            /*dateTimeLabelFormats:
+            {               //referance =>> https://stackoverflow.com/questions/7101464/how-to-get-highcharts-dates-in-the-x-axis
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '. %b',
+                week: '%e. %b',
+                month: '%b \'%y',
+                year: '%Y'
+            },*/
+            title:
+            {
+                text: 'Time'
+            }
+        },
+        yAxis:
+        {
+            title:
+            {
+                text: 'Snow depth (m)'
+            },
+            min: 0
+        },
+        tooltip:
+        {
+            headerFormat: '<b>{series.name} :<br/> {point.x:%e %b,%Y - %H:%M:%S}</b><br>',
+            pointFormat: '{point.y:.2f} unit'
+        },
+        series: get_ajax_data(ajax_source)
+    });
 }
 
-$(function()
+function get_ajax_data(ajax_source)
 {
-    if($("#chart_div").length != 0)
-    {
-      drawChart_totalUser('chart_div');
-    }
-});
+    var jsonData = $.ajax({
+                              url       : ajax_source,
+                              dataType  : "json",
+                              async     : false
+                            }).responseText;
 
-$( window ).resize(function()
-{
-    if($("#chart_div").length != 0)
+    var total_data_from_AJAX = [];
+    $.each($.parseJSON(jsonData), function(index, element)
     {
-      drawChart_totalUser('chart_div');
-    }
-});
+        var name = "";
+        var temp_AJAX_data=[];
+        $.each(element,function(key_array,value_array)
+        {
+            if(key_array.localeCompare('name')==0)
+            {
+                name=value_array;
+            }
+            else if(key_array.localeCompare('data')==0)
+            {
+                $.each(value_array,function(key_temp,value_temp)
+                {
+                    var time_year=0,time_month=0,time_day=0,time_hour=0,time_min=0,time_sec=0,value_of_data=0;
+                    $.each(value_temp,function(key,value)
+                    {
+                        if(key.localeCompare('time_year')==0)
+                        {
+                            time_year=value;
+                        }
+                        else if(key.localeCompare('time_month')==0)
+                        {
+                            time_month=value;
+                        }
+                        else if(key.localeCompare('time_day')==0)
+                        {
+                            time_day=value;
+                        }
+                        else if(key.localeCompare('time_hour')==0)
+                        {
+                            time_hour=value;
+                        }
+                        else if(key.localeCompare('time_min')==0)
+                        {
+                            time_min=value;
+                        }
+                        else if(key.localeCompare('time_sec')==0)
+                        {
+                            time_sec=value;
+                        }
+                        else if(key.localeCompare('value')==0)
+                        {
+                            value_of_data=value;
+                        }
+                    });
+                    temp_AJAX_data.push([Date.UTC(time_year, time_month,time_day,time_hour,time_min,time_sec), value_of_data]);
+                });
+            }
+        });
+        total_data_from_AJAX.push(
+                                    {
+                                        name: name,
+                                        data: temp_AJAX_data
+                                    }
+                                );
+    });
 
-setInterval(function()
-{
-    if($("#chart_div").length != 0)
-    {
-      drawChart_totalUser('chart_div');
-    }
-}, 20000);
+    return total_data_from_AJAX;
+}
